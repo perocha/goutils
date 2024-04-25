68e98e992a0b69b0be1f00e97caf0b5760da5ee5
Pedro Rocha <pedrojrocha@outlook.com>
2024-04-25 08:49:31 +0200

    Add new telemetry object using Zap

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.2
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/data/azappconfig v1.1.0
 	github.com/microsoft/ApplicationInsights-Go v0.4.4
+	go.uber.org/zap v1.27.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -13,6 +14,12 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.9.1 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.1 // indirect
 	github.com/gofrs/uuid v3.3.0+incompatible // indirect
-	golang.org/x/net v0.19.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
---
+++ pkg/telemetry/ztelemetry.go
@@ -0,0 +1,35 @@
+package telemetry
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+)
+
+type ZTelemetry struct {
+	logger *zap.Logger
+}
+
+func NewZTelemetry() (*ZTelemetry, error) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return nil, err
+	}
+	return &ZTelemetry{logger: logger}, nil
+}
+
+func (z *ZTelemetry) Info(ctx context.Context, msg string, tags ...zap.Field) {
+	operationID, _ := ctx.Value(OperationIDKeyContextKey).(string)
+	tags = append(tags, zap.String(string(OperationIDKeyContextKey), operationID))
+	z.logger.Info(msg, tags...)
+}
+
+func (z *ZTelemetry) Error(ctx context.Context, msg string, tags ...zap.Field) {
+	operationID, _ := ctx.Value(OperationIDKeyContextKey).(string)
+	tags = append(tags, zap.String(string(OperationIDKeyContextKey), operationID))
+	z.logger.Error(msg, tags...)
+}
+
+func (z *ZTelemetry) Sync() error {
+	return z.logger.Sync()
+}
